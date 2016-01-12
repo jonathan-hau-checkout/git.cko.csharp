18bb0137f77cb03c1c484f6c1c865e7d9603aff6
Jon Hau <jonathan.hau@checkout.com>
2016-01-12 09:32:28 +0800

    RISK SIMULATION
    Added risk simulation page.

---
+++ ckosite/api/simulations/riskMismatch.aspx
@@ -0,0 +1,153 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="riskMismatch.aspx.cs" Inherits="RiskMismatch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Mismatch Simulations</title>
+    <style type="text/css">
+
+        .auto-style1 {
+            width: 741px;
+            height: 451px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1 style="box-sizing: border-box; margin: 10px 0px 30px; -webkit-font-smoothing: antialiased; font-style: normal; font-family: 'Open Sans Light', Helvetica, sans-serif; font-size: 24px; font-weight: 300; color: rgb(37, 86, 160); font-variant: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255);">Mismatch Simulations</h1>
+    
+        <div>
+            
+            1. Fill in some details<br />
+            <br />
+            Name:<br />
+            <asp:TextBox ID="tb_custName" runat="server" Width="418px">Mr Test</asp:TextBox>
+            <br />
+            <br />
+            Email:<br />
+            <asp:TextBox ID="tb_custEmail" runat="server" Width="418px">a@internet.com</asp:TextBox>
+            <br />
+            <br />
+            Select Test Card:<br />
+            <asp:DropDownList ID="dropdown_testCard" runat="server" Width="418px">
+                <asp:ListItem Value="Visa01">Visa | 4242424242424242 | Exp 0618 | CVV 100</asp:ListItem>
+                <asp:ListItem Value="Visa02">Visa | 4543474002249996 | Exp 0617 | CVV 956</asp:ListItem>
+                <asp:ListItem Value="Master01">Master | 5436031030606378 | Exp 0617 | CVV 257</asp:ListItem>
+                <asp:ListItem Value="Amex01">Amex | 345678901234564 | Exp 0617 | CVV 1051</asp:ListItem>
+                <asp:ListItem Value="Amex02">Amex | 378282246310005 | Exp 0618 | CVV 1000</asp:ListItem>
+                <asp:ListItem Value="Diners01">Diners | 30123456789019 | Exp 0617 | CVV 257</asp:ListItem>
+                <asp:ListItem Value="Jcb01">JCB | 3530111333300000 | Exp 0618 | CVV 100</asp:ListItem>
+                <asp:ListItem Value="Discover01">Discover | 6011111111111117 | Exp 0618 | CVV 100</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            Value:<br />
+            <asp:TextBox ID="tb_value" runat="server" Width="200px">100</asp:TextBox>
+            <br />
+            <br />
+            Currency:<br />
+            <asp:TextBox ID="tb_currency" runat="server" Width="200px">SGD</asp:TextBox>
+            <br />
+            <br />
+            Billing Descriptor:<br />
+            - City
+            (max 13 characters)<br />
+            <asp:TextBox ID="tb_descriptorCity" runat="server" Width="300px">Singapore</asp:TextBox>
+            <br />
+            - Name (First 21 characters are safe from truncation)<br />
+            <asp:TextBox ID="tb_descriptorName" runat="server" Width="300px">Jonathan Toys</asp:TextBox>
+            <br />
+            <br />
+            <asp:CheckBox ID="cb_autocap" runat="server" Text="Autocapture?" />
+            <br />
+            <br />
+            <br />
+            2. Let&#39;s simulate ...<br />
+            <br />
+            <asp:DropDownList ID="dd_shipCountry" runat="server" Height="16px" Width="153px">
+                <asp:ListItem Value="AU">Australia</asp:ListItem>
+                <asp:ListItem Value="BE">Belgium</asp:ListItem>
+                <asp:ListItem Value="CN">China</asp:ListItem>
+                <asp:ListItem Value="JP">Japan</asp:ListItem>
+                <asp:ListItem Value="SG">Singapore</asp:ListItem>
+                <asp:ListItem Value="UK">United Kingdom</asp:ListItem>
+                <asp:ListItem Value="US">United States</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;Shipping Country&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:DropDownList ID="dd_billCountry" runat="server" Height="16px" Width="153px">
+                <asp:ListItem Value="AU">Australia</asp:ListItem>
+                <asp:ListItem Value="BE">Belgium</asp:ListItem>
+                <asp:ListItem Value="CN">China</asp:ListItem>
+                <asp:ListItem Value="JP">Japan</asp:ListItem>
+                <asp:ListItem Value="SG">Singapore</asp:ListItem>
+                <asp:ListItem Value="UK">United Kingdom</asp:ListItem>
+                <asp:ListItem Value="US">United States</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;Billing Country<br />
+            <asp:DropDownList ID="dd_binCountry" runat="server" Height="16px" Width="153px">
+                <asp:ListItem Value="AU">Australia</asp:ListItem>
+                <asp:ListItem Value="BE">Belgium</asp:ListItem>
+                <asp:ListItem Value="CN">China</asp:ListItem>
+                <asp:ListItem Value="JP">Japan</asp:ListItem>
+                <asp:ListItem Value="SG">Singapore</asp:ListItem>
+                <asp:ListItem Value="UK">United Kingdom</asp:ListItem>
+                <asp:ListItem Value="US">United States</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;BIN Country (not used at the moment. Not sure how to simulate this.)<br />
+            <asp:DropDownList ID="dd_ipCountry" runat="server" Height="16px" Width="153px">
+                <asp:ListItem Value="14.137.128.0">Australia</asp:ListItem>
+                <asp:ListItem Value="31.31.128.0">Belgium</asp:ListItem>
+                <asp:ListItem Value="14.144.0.0">China</asp:ListItem>
+                <asp:ListItem Value="14.128.64.0">Japan</asp:ListItem>
+                <asp:ListItem Value="118.200.158.50">Singapore</asp:ListItem>
+                <asp:ListItem Value="5.10.16.0">United Kingdom</asp:ListItem>
+                <asp:ListItem Value="13.128.0.0">United States</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;IP Country<br />
+            <asp:DropDownList ID="dd_phoneBillCountry" runat="server" Height="16px" Width="153px">
+                <asp:ListItem Value="61">Australia</asp:ListItem>
+                <asp:ListItem Value="32">Belgium</asp:ListItem>
+                <asp:ListItem Value="86">China</asp:ListItem>
+                <asp:ListItem Value="81">Japan</asp:ListItem>
+                <asp:ListItem Value="65">Singapore</asp:ListItem>
+                <asp:ListItem Value="44">United Kingdom</asp:ListItem>
+                <asp:ListItem Value="1">United States</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;Billing Phone Country<br />
+            <asp:DropDownList ID="dd_phoneShipCountry" runat="server" Height="16px" Width="153px">
+                <asp:ListItem Value="61">Australia</asp:ListItem>
+                <asp:ListItem Value="32">Belgium</asp:ListItem>
+                <asp:ListItem Value="86">China</asp:ListItem>
+                <asp:ListItem Value="81">Japan</asp:ListItem>
+                <asp:ListItem Value="65">Singapore</asp:ListItem>
+                <asp:ListItem Value="44">United Kingdom</asp:ListItem>
+                <asp:ListItem Value="1">United States</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;Shipping Phone Country<br />
+            <br />
+            <br />
+            <asp:Button ID="but_chargeUsingCardId" runat="server" OnClick="but_chargeUsingCardId_Click" Text="Simulate!" />
+            <br />
+            <br />
+            <br />
+            <br />
+            3. Response<br />
+            <br />
+            <asp:TextBox ID="tb_chargeRsp" runat="server" Height="238px" ReadOnly="True" TextMode="MultiLine" Width="900px"></asp:TextBox>
+            <br />
+            <br />
+            
+            <img alt="" class="auto-style1" src="images/riskMismatch.png" /><br />
+            <br />
+            
+        </div>
+<hr />
+    
+    </div>
+    </form>
+    </body>
+</html>
---
+++ ckosite/api/simulations/riskMismatch.aspx.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Checkout;
+using Checkout.ApiServices.SharedModels;
+using Checkout.ApiServices.Charges.RequestModels;
+using Checkout.ApiServices.Charges.ResponseModels;
+using Checkout.ApiServices.Cards.ResponseModels;
+using Checkout.ApiServices.Cards.RequestModels;
+
+
+public partial class RiskMismatch : System.Web.UI.Page
+{
+    protected APIClient ckoAPIClient;
+
+    protected string expiryMonth;
+
+    protected string expiryYear;
+
+    protected string cvv;
+
+    protected string cardNum;
+
+    private string[] RandomString(int length1, int length2)
+    {
+        var ran = new Random();
+        string[] custname = new string[2];
+
+        custname[0] = new string(Enumerable.Range(0, length1).Select(_ => (char)ran.Next('a', 'n')).ToArray());
+        custname[1] = new string(Enumerable.Range(0, length2).Select(_ => (char)ran.Next('o', 'z')).ToArray()); ;
+
+        return custname;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+            return;
+
+        // generates random customer name
+        string[] custname = RandomString(3, 5);
+
+        tb_custName.Text = string.Format("{0} {1}", custname[0].Substring(0, 1).ToUpper() + custname[0].Substring(1),
+                                                    custname[1].Substring(0, 1).ToUpper() + custname[1].Substring(1));
+
+        // generates random email
+        tb_custEmail.Text = string.Format("{0}.{1}@checkout.com", custname[0], custname[1]);
+
+    }
+
+    protected void but_chargeUsingCardId_Click(object sender, EventArgs e)
+    {
+        // determines selected card
+        switch (dropdown_testCard.SelectedValue)
+        {
+            case "Visa01":
+                expiryMonth = "06";
+                expiryYear = "2018";
+                cvv = "100";
+                cardNum = "4242424242424242";
+                break;
+            case "Visa02":
+                expiryMonth = "06";
+                expiryYear = "2017";
+                cvv = "956";
+                cardNum = "4543474002249996";
+                break;
+            case "Master01":
+                expiryMonth = "06";
+                expiryYear = "2017";
+                cvv = "257";
+                cardNum = "5436031030606378";
+                break;
+            case "Amex01":
+                expiryMonth = "06";
+                expiryYear = "2017";
+                cvv = "1051";
+                cardNum = "345678901234564";
+                break;
+            case "Amex02":
+                expiryMonth = "06";
+                expiryYear = "2018";
+                cvv = "1000";
+                cardNum = "378282246310005";
+                break;
+            case "Diners01":
+                expiryMonth = "06";
+                expiryYear = "2017";
+                cvv = "257";
+                cardNum = "30123456789019";
+                break;
+            case "Jcb01":
+                expiryMonth = "06";
+                expiryYear = "2018";
+                cvv = "100";
+                cardNum = "3530111333300000";
+                break;
+            case "Discover01":
+                expiryMonth = "06";
+                expiryYear = "2018";
+                cvv = "100";
+                cardNum = "6011111111111117";
+                break;
+            default:
+                expiryMonth = "06";
+                expiryYear = "2018";
+                cvv = "100";
+                cardNum = "4242424242424242";
+                break;
+        }
+
+        /* Country      IP              ISO2 Code   Dial Code
+         * Australia    14.137.128.0    AU          61    
+         * Belgium      31.31.128.0     BE          32
+         * China        14.144.0.0      CN          86
+         * Japan        14.128.64.0     JP          81
+         * Singapore    118.200.158.50  SG          65
+         * UK           5.10.16.0       GB          44      
+         * USA          13.128.0.0      US          1
+         */
+
+
+        // Creates payload
+        var cardRequestModel = new CardCharge()
+        {
+            AutoCapture = cb_autocap.Checked?"Y":"N",
+            CustomerIp = dd_ipCountry.SelectedValue,
+            ShippingDetails = new Address()
+            {
+                AddressLine1 = "321 Ship Sim Road",
+                AddressLine2 = "Ship Sim Building",
+                City = "Ship Sim City",
+                Country = dd_shipCountry.SelectedValue,
+                Phone = new Phone()
+                {
+                    CountryCode = dd_phoneShipCountry.SelectedValue,
+                    Number = "9876543"
+                },
+                Postcode = "888888",
+                State = "Ship Sim State"
+            },
+            AutoCapTime = 0,
+            Currency = tb_currency.Text,
+            TrackId = "TRK12345",
+            TransactionIndicator = "1",
+            Description = "Ipad for Ebs travel",
+            Value = tb_value.Text,
+            Email = tb_custEmail.Text,
+            Card = new CardCreate()
+            {
+                ExpiryMonth = expiryMonth,
+                ExpiryYear = expiryYear,
+                Cvv = cvv,
+                Number = cardNum,
+                Name = tb_custName.Text,
+                BillingDetails = new Address()
+                {
+                    AddressLine1 = "123 Bill Sim Road",
+                    AddressLine2 = "Bill Sim Building",
+                    Postcode = "999999",
+                    City = "Bill Sim City",
+                    State = "Bill Sim State",
+                    Country = dd_billCountry.SelectedValue,
+                    Phone = new Phone()
+                    {
+                        CountryCode = dd_phoneBillCountry.SelectedValue,
+                        Number = "12345678"
+                    }
+                }
+            },
+            Descriptor = new BillingDescriptor()
+            {
+                City = tb_descriptorCity.Text,
+                Name = tb_descriptorName.Text
+            },
+            Products = new List<Product>{ new Product(){
+                                             Description = "item 1",
+                                             Name = "item 1",
+                                             Price = 33.5M,
+                                             Quantity = 3,
+                                             ShippingCost = 5.5M,
+                                             Sku = "sku123"
+                                            }, 
+                                            new Product(){
+                                             Description = "item 2",
+                                             Name = "item 2",
+                                             Price = 31.5M,
+                                             Quantity = 3,
+                                             ShippingCost = 4.5M,
+                                             Sku = "sku456"
+                                            }
+            },
+
+
+            Metadata = new Dictionary<string, string>() { { "extraInformation", "EBS travel" } },
+            Udf1 = "udf1 string",
+            Udf2 = "udf2 string",
+            Udf3 = "udf3 string",
+            Udf4 = "udf4 string",
+            Udf5 = "udf5 string"
+        };
+
+        try
+        {
+            // Create APIClient instance with your secret key
+            ckoAPIClient = new APIClient();
+
+            // Submit your request and receive an apiResponse
+            HttpResponse<Charge> apiResponse = ckoAPIClient.ChargeService.ChargeWithCard(cardRequestModel);
+
+            if (!apiResponse.HasError)
+            {
+                // Access the response object retrieved from the api
+                var charge = apiResponse.Model;
+
+                tb_chargeRsp.Text = String.Format("Status:\n {0}! \n\nCharge ID:\n {1} \n\nCustomerId:\n {2} \n\nCard Id:\n {3} \n\nLast 4 Card Numbers:\n {4} \n\nCharge Response:\n {5} \n\nJSON Response: \n {6}",
+                    charge.Status, charge.Id, charge.Card.CustomerId, charge.Card.Id, charge.Card.Last4, charge.ResponseMessage, apiResponse.FullJsonResponse);
+            }
+            else
+            {
+                // Api has returned an error object. You can access the details in the error property of the apiResponse.
+                // apiResponse.error
+                tb_chargeRsp.Text = string.Format("Event ID:\n{0} \n\nMessage:\n{1} \n\nError Code:\n{2} \n\nErrors:\n{3}",
+                    apiResponse.Error.EventId, apiResponse.Error.Message, apiResponse.Error.ErrorCode, apiResponse.Error.Errors);
+            }
+        }
+        catch (Exception exc)
+        {
+            //... Handle exception
+        }
+    }
+
+
+}
